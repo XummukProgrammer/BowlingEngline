31317071311fe9c760d4d172ca9a6e25416446ee
Гребенщиков Андрей <a-grebenshikov@bk.ru>
2025-04-04 20:34:17 +0500

    В экран загрузки выведена информация о проекте

--- Assets/Fun2Games/UnityGameTemplate/Scripts/Runtime/UI/Windows/Common/LoadWindow/UGTLoadWindowService.cs
+++ Assets/Fun2Games/UnityGameTemplate/Scripts/Runtime/UI/Windows/Common/LoadWindow/UGTLoadWindowService.cs
@@ -1,3 +1,4 @@
+using UnityGameTemplate.Starter.Models;
 using UnityGameTemplate.UI.Windows.Models;
 using UnityGameTemplate.UI.Windows.Services;
 
@@ -8,13 +9,26 @@ namespace UnityGameTemplate.UI.Windows.Common.LoadWindow
         public override UGTWindowModel Model => _model;
 
         private readonly UGTLoadWindowModel _model;
+        private readonly UGTProjectModel _projectModel;
 
         public UGTLoadWindowService(
             UGTWindowContainerService containerService,
-            UGTLoadWindowModel model) 
+            UGTLoadWindowModel model,
+            UGTProjectModel projectModel) 
             : base(containerService)
         {
             _model = model;
+            _projectModel = projectModel;
+        }
+
+        public override void OnViewInstantiate()
+        {
+            base.OnViewInstantiate();
+
+            var view = View as UGTLoadWindowView;
+            view.GameName = _projectModel.Name;
+            view.Version = _projectModel.FullVersion;
+            view.CompanyName = "Fun2Games";
         }
     }
 }
--- Assets/Fun2Games/UnityGameTemplate/Scripts/Runtime/UI/Windows/Common/LoadWindow/UGTLoadWindowView.cs
+++ Assets/Fun2Games/UnityGameTemplate/Scripts/Runtime/UI/Windows/Common/LoadWindow/UGTLoadWindowView.cs
@@ -1,8 +1,35 @@
+using UnityEngine;
 using UnityGameTemplate.UI.Windows.Views;
 
 namespace UnityGameTemplate.UI.Windows.Common.LoadWindow
 {
     public class UGTLoadWindowView : UGTWindowView
     {
+        public string GameName
+        {
+            get => _gameNameText.text;
+            set => _gameNameText.text = value;
+        }
+
+        public string Version
+        {
+            get => _versionText.text;
+            set => _versionText.text = value;
+        }
+
+        public string CompanyName
+        {
+            get => _companyNameText.text;
+            set => _companyNameText.text = value;
+        }
+
+        [SerializeField]
+        private TMPro.TMP_Text _gameNameText;
+
+        [SerializeField]
+        private TMPro.TMP_Text _versionText;
+
+        [SerializeField]
+        private TMPro.TMP_Text _companyNameText;
     }
 }
